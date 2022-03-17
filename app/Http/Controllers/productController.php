9ec951a60ed8b0a8e59448b2a518879ff5ab7af1
Manauar Aly <you@example.com>
2022-03-17 18:51:28 +0530

    Demi data

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\product;
 
 class productController extends Controller
 {
     function index(){
-        return 'welcome to product page...';
+        
+        $product = product::all();
+        return view('product',['products'=>$product]);
     }
 }
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,10 +3,11 @@
     <head>
         <meta charset="UTF-8">
         <title>Document</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>    
+        <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     </head>
     <body>
         {{View::make('header')}}
@@ -17,6 +18,10 @@
         .custom-login{
             height: 500px;
             padding-top: 100px;
+        }
+        .ced-slider{
+            height:500px !important;
+            width:100%;
         }
     </style>
 </html>
